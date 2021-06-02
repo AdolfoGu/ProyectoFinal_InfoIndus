719c4a2455f591bdd8ebe5d5596334ae1208d87b
Adolfo Gutierrez <ja.gutierrez.gaytan@ugto.mx>
2021-06-02 18:54:48 -0500

    Calculo de la distancia

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,12 +1,27 @@
 //pines del sensor HC-SR04
 const int trigPin = 9;
 const int echoPin = 9;
-
+long duracion;
+int distancia;
 void setup() {
-  
+  //incializamos un pin de salida y uno de entrada
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Apagamos el sensor para evitar errores
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
+  //Ensendemos el sensor por 10 milis y apagamos
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duracion = pulseIn(echoPin, HIGH);
+  //calculo de la distancia en cm
+  distancia = (duracion * 0.034) / 2;
+  
 }
